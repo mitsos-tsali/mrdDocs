5cbb1071ebbc1d01eceac487f72803fd869f9978
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2020-12-12 13:26:59 +0200

    Added docs!

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pandas as pd
 import os
 import glob
 
+'''
+Programm concating given excel files into one.
+In order to run, file "Docs" must be created in the directory.
+axis=0 by default, if axis=1, concat will be done horizontally
+'''
+
 
 def run():
     if len(os.listdir('Docs')) == 0:
@@ -9,7 +15,7 @@ def run():
     else:
         df_list = []
         for filename in glob.glob("Docs/*.xlsx"):
-            temp_df = pd.read_excel(filename,skiprows=1)
+            temp_df = pd.read_excel(filename, skiprows=1)
             df_list.append(temp_df)
         df = pd.concat(df_list)
         df.to_excel("Final.xlsx")
